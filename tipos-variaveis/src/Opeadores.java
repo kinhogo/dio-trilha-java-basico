0d074dca892092ee551b713a8ea5ac5160603d85
liniker_viana <linikerti@gmail.com>
2024-05-07 21:12:04 -0300

    update tipos e variaveis e adição de operadores

---
+++ tipos-variaveis/src/Opeadores.java
@@ -0,0 +1,24 @@
+public class Opeadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" +" "+ "JAVA"; // + É O OPERADOR DE JUNÇÃO
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+    }
+    
+}
